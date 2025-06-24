0a6db9ecaa667169138fdb8aa3f8c8ea5ef80422
Deepak Dhiman <99280200+Deepak-Dhiman@users.noreply.github.com>
2025-06-24 10:54:47 +0530

    Edit repeatString.js

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  let returned = "";
+  for (let i = 0; i < num; i++) {
+    returned += string;
+  }
+  return returned;
 };
+
+str = "hey";
+
+console.log(repeatString(str, 3));
 
 // Do not edit below this line
 module.exports = repeatString;
