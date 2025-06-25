af99338d50246979b15331b22faf4f32a0d19550
Deepak Dhiman <99280200+Deepak-Dhiman@users.noreply.github.com>
2025-06-26 02:27:32 +0530

    Edit tempConversion.js

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temp) {
+  let C = ((temp - 32) * 5) / 9;
+  Math.round(C * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+  let F = (temp * 9) / 5 + 32;
+  Math.round(F * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
