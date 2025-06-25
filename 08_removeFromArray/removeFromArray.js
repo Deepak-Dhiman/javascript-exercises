985caa96fd4b2193df61ce4e3ccfbe0234c5add0
Deepak Dhiman <99280200+Deepak-Dhiman@users.noreply.github.com>
2025-06-26 01:43:30 +0530

    Edit  removeFromArray.js

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, x) {
+  let y;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === x) y = i;
+  }
+  arr.splice(y, 0);
+  console.log(arr);
 };
+
+let arr = [1, 2, 3, 4];
+
+removeFromArray(arr, 3);
 
 // Do not edit below this line
 module.exports = removeFromArray;
