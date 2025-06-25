0c769a5eec7c9f1597e5b4ec822af381f761bac3
Deepak Dhiman <99280200+Deepak-Dhiman@users.noreply.github.com>
2025-06-26 02:12:47 +0530

    Edit leapYears.js

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
-
+const leapYears = function (num) {
+  if (num % 400 === 0) {
+    console.log("Leap year!");
+  } else if (num % 100 === 0) {
+    console.log("Not a leap year!");
+  } else {
+    console.log("Leap Year");
+  }
 };
 
 // Do not edit below this line
